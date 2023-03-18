0bc9589c2cbda6375b10756e0f12b0d864778246
장우석 <wkddntjr1123@gmail.com>
2023-03-18 23:33:37 +0900

    Time: 244 ms (65.83%), Space: 37.7 MB (83.89%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.kt
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.kt
@@ -1,11 +1,7 @@
 class Solution {
     fun removeDuplicates(nums: IntArray): Int {
-        var counter = 0      
-        for(i in 1 until nums.size){
-            if(nums[i] > nums[counter]){
-                nums[++counter] = nums[i]
-            }
-        }
-        return counter + 1
+        var counter = 0
+        for (i in nums.indices) if (nums[i] != nums[counter]) nums[++counter] = nums[i]
+        return ++counter
     }
 }
