e777eaab06564534e930d9351fa9dab526df74f6
장우석 <wkddntjr1123@gmail.com>
2023-03-19 00:42:49 +0900

    Time: 366 ms, Memory: 39.2 MB - LeetHub

--- 0max-consecutive-ones/0max-consecutive-ones.kt
+++ 0max-consecutive-ones/0max-consecutive-ones.kt
@@ -2,16 +2,6 @@ import kotlin.math.max
 
 class Solution {
     fun findMaxConsecutiveOnes(nums: IntArray): Int {
-        var count = 0
-        var answer = 0
-        for(num in nums){
-            if(num == 1) count++
-            else {
-                answer = max(answer, count)
-                count = 0
-            }
-        }
-        answer = max(answer, count)
-        return answer
+        return nums.joinToString("").split("0").maxBy {it.length}!!.length
     }
 }
