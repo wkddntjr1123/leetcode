6a9922378188524444a79198035a61245cf91b19
장우석 <wkddntjr1123@gmail.com>
2023-04-01 22:39:40 +0900

    Time: 163 ms (85.43%), Space: 35 MB (99.34%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.kt
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.kt
@@ -4,17 +4,15 @@ class Solution {
             '2' to "abc", '3' to "def", '4' to "ghi", '5' to "jkl",
             '6' to "mno", '7' to "pqrs", '8' to "tuv", '9' to "wxyz")
         val result = mutableListOf<String>()
-        
-        tailrec fun dfs(level: Int, cand: String) {
-            if(level > digits.length - 1) {
-                result.add(cand)
-                return
-            }
-            for(c in table[digits[level]]!!) {
-                dfs(level + 1, cand + c)            
+        if (digits.isEmpty()) return result else result.add("")
+        for ((i, digit) in digits.withIndex()) {
+            while(result[0].length != i + 1) {
+                val base = result.removeAt(0)
+                for(c in table[digit]!!) {
+                    result.add(base + c)
+                }
             }
         }
-        if(digits.length != 0) dfs(0, "")
         return result
     }
 }
