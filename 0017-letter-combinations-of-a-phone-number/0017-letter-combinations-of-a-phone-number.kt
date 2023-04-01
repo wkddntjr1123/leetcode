0cd79bbe4f080394072c90824ca6b802e08f8548
장우석 <wkddntjr1123@gmail.com>
2023-04-01 21:07:57 +0900

    Time: 146 ms (99.01%), Space: 35.3 MB (98.01%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun letterCombinations(digits: String): List<String> {
+        val table = mapOf(
+            '2' to "abc", '3' to "def", '4' to "ghi", '5' to "jkl",
+            '6' to "mno", '7' to "pqrs", '8' to "tuv", '9' to "wxyz")
+        val result = mutableListOf<String>()
+        
+        tailrec fun dfs(level: Int, cand: String) {
+            if(level > digits.length - 1) {
+                result.add(cand)
+                return
+            }
+            for(c in table[digits[level]]!!) {
+                dfs(level + 1, cand + c)            
+            }
+        }
+        if(digits.length == 0) return result
+        dfs(0, "")
+        return result
+    }
+}
